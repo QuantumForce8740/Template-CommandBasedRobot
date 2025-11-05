d5fba3f9886aa654c7f9ecb561c32bc71754e1e6
SaraEster1234 <sarita02gtz.@gmail.com>
2025-11-04 19:06:20 -0600

    cambios
    mi parte de lo del drive

--- src/main/cpp/Chasis.cpp
+++ src/main/cpp/Chasis.cpp
@@ -1,18 +1,35 @@
 #include "Chasis.h"
+#include <utility>
 
-Chasis::Chasis()
-{
-    m_leftBack.Follow(m_leftFront);
-    m_rightBack.Follow(m_rightFront);
+#include <Robot.h>
 
-    m_rightFront.SetInverted(true);
-    m_leftBack.SetInverted(true); 
 
-    
+
+Chasis::Chasis(){
+
+
+
+    void Chasis::SetMotors(double leftSpeed, double rightSpeed) {
+
+
+
+         m_leftFront.Set(leftSpeed);
+        m_RightFront.Set(RightSpeed);
+    }   
 }
 
-void Chasis::SetMotors(double leftSpeed, double rightSpeed) {
 
-    m_leftFront.Set(leftSpeed);
-    m_RightFront.Set(RightSpeed);
+
+
+DefaultDrive::DefaultDrive(DriveSubsystem* subsystem,
+                           std::function<double()> forward,
+                           std::function<double()> rotation)
+    : m_drive{subsystem},
+      m_forward{std::move(forward)},
+      m_rotation{std::move(rotation)} {
+  AddRequirements(subsystem);
+}
+
+void DefaultDrive::Execute() {
+  m_drive->ArcadeDrive(m_forward(), m_rotation());
 }
